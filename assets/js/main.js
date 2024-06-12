b43521ab0f25ededa67cf93a941c1f17b5047a18
ercoppa <ercoppa@gmail.com>
2024-06-12 17:56:37 +0200

    update

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+var app = new Vue({
+
+    el: '#app',
+
+    data: {
+        publications: [],
+        citations: [],
+        showBib: -1
+    },
+
+    methods: {
+
+        fetchPublications: function (path, vue) {
+
+            const Cite = require('citation-js')
+            $.get(path, function (data) {
+                vue.citations = new Cite(data);
+                vue.publications = vue.citations.data;
+            })
+        },
+
+        printBib: function (c) {
+            const Cite = require('citation-js');
+            return (new Cite(c)).format('bibtex').replace(/\t/g, " ");
+        },
+
+        showBibEntry: function (k) {
+            if (this.showBib == k)
+                this.showBib = -1;
+            else
+                this.showBib = k;
+        }
+    },
+
+    created() {
+        this.fetchPublications('assets/bib/publications.bib', this);
+    }
+
+});
+
